538e34b96310f1dd13bd2766f07299b53f65febb
Alka Lama <82252528+alkawaiba@users.noreply.github.com>
2022-01-03 11:44:08 +0545

    Add files via upload

---
+++ BKSRecreationalCentre/BKSRecreationalCentre/WDTktRate.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BKSRecreationalCentre
+{
+     public class WDTktRate
+    {
+        public string TktType { get; set; }
+        public float WDRate1Hour {get; set;}
+        public float WDRate2Hours { get; set; }
+        public float WDRate3Hours { get; set; }
+        public float WDRate4Hours { get; set; }
+        public float WDRateWholeDay { get; set; }
+        
+    }
+}
